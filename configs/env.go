d9ab3de6af413f705f68faf54f01ad5155b6daf7
walid moultamiss <walidmoultamis@gmail.com>
2022-04-22 02:23:29 +0000

    REST API

---
+++ configs/env.go
@@ -0,0 +1,16 @@
+package configs
+
+import (
+	"github.com/joho/godotenv"
+	"log"
+	"os"
+)
+
+func EnvMongoURI() string {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	return os.Getenv("MONGOURI")
+}
